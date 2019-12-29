73f7a5e82bf7ccb2f0c10d4f8bea748158f1d6e7
Win <Ritikguptasr@gmail.com>
2019-12-29 15:01:45 +0530

    Social Media Activity Implement

--- app/src/main/java/com/example/insta/SocialMediaActivity.java
+++ app/src/main/java/com/example/insta/SocialMediaActivity.java
@@ -10,5 +10,6 @@ public class SocialMediaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        setTitle("Social Media Activity");
     }
 }
