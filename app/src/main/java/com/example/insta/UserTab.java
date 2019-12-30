2735d006d5bf120ce8365dd70383a583b4abab61
Win <Ritikguptasr@gmail.com>
2019-12-30 17:27:20 +0530

    ArrayList and ArrayAdapter

--- app/src/main/java/com/example/insta/UserTab.java
+++ app/src/main/java/com/example/insta/UserTab.java
@@ -8,12 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserTab extends Fragment {
+    private ListView listView;
+    private ArrayList arrayList;
+    // ArrayAdapter for displaying the data
+    private ArrayAdapter arrayAdapter;
 
 
     public UserTab() {
@@ -25,7 +39,31 @@ public class UserTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_tab, container, false);
+        listView = view.findViewById(R.id.listView);
+        // Initialize array
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arrayList);
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        // condition for not including the current user in the list
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e == null){
+                    if(objects.size() > 0){
+                        for (ParseUser user : objects ){
+                            arrayList.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+                    }
+                }
+            }
+        });
+
+        return view;
+
+
     }
 
 }
